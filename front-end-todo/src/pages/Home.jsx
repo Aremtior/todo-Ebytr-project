7509922824255db1c2a3f691bbe7a5465eb41154
Aremtior <marcosleandrofl@gmail.com>
2021-11-04 15:52:03 -0300

    BtnRemoveTask.jsx was created.

--- front-end-todo/src/pages/Home.jsx
+++ front-end-todo/src/pages/Home.jsx
@@ -1,4 +1,3 @@
-
 import Header from '../components/Header';
 import NewTask from '../components/NewTask';
 import TaskList from '../components/TaskList';
@@ -6,11 +5,11 @@ import TaskList from '../components/TaskList';
 const Home = () => {
   return (
     <div>
-      <Header title='To do List' />
+      <Header title="To do List" />
       <NewTask />
       <TaskList />
     </div>
   );
-}
+};
 
 export default Home;
