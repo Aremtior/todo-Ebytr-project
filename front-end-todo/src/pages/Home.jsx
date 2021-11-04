e4bfc35871367e56fb1d71b2d2fa6fa123ada28a
Aremtior <marcosleandrofl@gmail.com>
2021-11-04 15:31:13 -0300

    NewTask.jsx component created with styles and added to Home.jsx page.

--- front-end-todo/src/pages/Home.jsx
+++ front-end-todo/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 
 import Header from '../components/Header';
+import NewTask from '../components/NewTask';
 
 const Home = () => {
   return (
     <div>
       <Header title='To do List' />
+      <NewTask />
     </div>
   );
 }
